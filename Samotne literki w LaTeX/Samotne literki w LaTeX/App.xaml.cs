cc225ac0b31face242afbfbd9c62293226ee8484
rewolt <rewolt90@gmail.com>
2015-05-31 19:31:02 +0200

    Nowy projekt "Samotne literki w LaTeX.sln"

---
+++ Samotne literki w LaTeX/Samotne literki w LaTeX/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Samotne_literki_w_LaTeX
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            bool startMinimized = false;
+            for (int i = 0; i != e.Args.Length; ++i)
+            {
+                if (e.Args[i] == "/StartMinimized")
+                {
+                    startMinimized = true;
+                }
+            }
+            // Create main application window, starting minimized if specified
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
