91b9f640aa591bedd0ec9ca145638677b79dcd39
Bartek <rewolt90@gmail.com>
2015-06-02 13:25:55 +0200

    Dodanie metod do wczytywania i zapisu

--- Samotne literki w LaTeX/Samotne literki w LaTeX/MainWindow.xaml.cs
+++ Samotne literki w LaTeX/Samotne literki w LaTeX/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,10 @@ namespace Samotne_literki_w_LaTeX
     /// </summary>
     public partial class MainWindow : Window
     {
+        string odczytLokalizacja = "";
+        string zapisLokalizacja = "";
+        string zapisBackupLokalizacja = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +32,47 @@ namespace Samotne_literki_w_LaTeX
 
         private void bt_konwertuj_Click(object sender, RoutedEventArgs e)
         {
-            
+            MessageBox.Show("chuj");
+        }
+
+        private void bt_wczytaj_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Plik .tex|*.tex";
+            openFile.Title = "Wybierz plik do odczytu...";
+            openFile.ShowDialog();
+            odczytLokalizacja = openFile.FileName;
+            tb_wczytaj.Text = odczytLokalizacja;
+            updateLokalizacji();
+        }
+
+        private void bt_zapisz_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Plik .tex|*.tex";
+            saveFile.Title = "Zapisz plik jako...";
+            saveFile.ShowDialog();
+            zapisLokalizacja = saveFile.FileName;
+            tb_zapisz.Text = zapisLokalizacja;
+        }
+
+        private void bt_zapisz_backup_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "Plik .tex|*.tex";
+            saveFile.Title = "Zapisz plik jako...";
+            saveFile.ShowDialog();
+            zapisBackupLokalizacja = saveFile.FileName;
+            tb_zapisz_backup.Text = zapisLokalizacja;
+        }
+
+        private void updateLokalizacji()
+        {
+            zapisLokalizacja = odczytLokalizacja;
+            zapisBackupLokalizacja = zapisLokalizacja;
+            zapisBackupLokalizacja = zapisBackupLokalizacja.Insert(zapisBackupLokalizacja.LastIndexOf('.'), "_BACKUP");
+            tb_zapisz.Text = zapisLokalizacja;
+            tb_zapisz_backup.Text = zapisBackupLokalizacja;
         }
     }
 }
