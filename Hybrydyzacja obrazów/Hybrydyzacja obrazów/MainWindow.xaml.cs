0fed04b76b912699d9b4234c3ef00d18859b910a
Bartek <rewolt90@gmail.com>
2015-06-04 16:53:53 +0200

    Nowy projekt "Hybrydyzacja obrazów"

---
+++ Hybrydyzacja obrazów/Hybrydyzacja obrazów/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
+
+namespace Hybrydyzacja_obrazów
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            byte[] imageBytes = LoadImageData(@"D:\Ściągnięte\HTO_zdjecia_pogrupowane\Admin\1.jpg");
+            BitmapSource imageSource = (BitmapSource)CreateImage(imageBytes, 640, 0);
+            ImageSource image = (ImageSource)imageSource;
+            imageBox.Source = image;
+           
+        }
+
+        private static byte[] LoadImageData(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            byte[] imageBytes = br.ReadBytes((int)fs.Length);
+            br.Close();
+            fs.Close();
+            return imageBytes;
+        }
+
+        private static ImageSource CreateImage(byte[] imageData, int decodePixelWidth, int decodePixelHeight)
+        {
+            if (imageData == null) return null;
+            BitmapImage result = new BitmapImage();
+            result.BeginInit();
+            if (decodePixelWidth > 0)
+            {
+                result.DecodePixelWidth = decodePixelWidth;
+            }
+            if (decodePixelHeight > 0)
+            {
+                result.DecodePixelHeight = decodePixelHeight;
+            }
+            result.StreamSource = new MemoryStream(imageData);
+            result.CreateOptions = BitmapCreateOptions.None;
+            result.CacheOption = BitmapCacheOption.Default;
+            result.EndInit();
+            return result;
+        }
+    }
+}
